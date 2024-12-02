836fff551ca9ea90ecb85866b611341f4cb82cf3
SukonlawatT <sugornawat.0903@gmail.com>
2024-12-02 13:23:22 +0700

    2/12/67

--- Lapsheet1/Lapsheet01/src/Lap101.java
+++ Lapsheet1/Lapsheet01/src/Lap101.java
@@ -9,13 +9,15 @@ public class Lap101 {
 		int unitName = scan.nextInt();
 		System.out.print("Input Price per unit : ");
 		float PriceperUnit = scan.nextFloat();
+		
 		System.out.println("---------------------------------------------------");
 		float totalPrice = unitName * PriceperUnit;
-		System.out.print("Total Price is : " + totalPrice + " baht.");
+		System.out.println("Total Price is : " + totalPrice + " baht.");
 		System.out.println("---------------------------------------------------");
 		System.out.print("How many discount (%) : ");
 		float Discount = scan.nextFloat();
 		System.out.println("---------------------------------------------------");
+		
 		float totalDiscount = totalPrice + (totalPrice * Discount/100);
 		//System.out.printl("Discount form " + Discount% + );
 		System.out.println("Amount to be paid      " + totalDiscount + " baht.");
