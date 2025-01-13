83e27384e2933a94447fe09801c5d28d34531957
SukonlawatT <sugornawat.0903@gmail.com>
2025-01-13 10:50:57 +0700

    Lab05

---
+++ JavaLabsheetAll/Labsheet05/Labsheet05/src/Lab503.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;	
+public class Lab503 {
+	public static void main(String[] args) {
+        
+        int[] numbers = new int[7];
+        Scanner scanner = new Scanner(System.in);
+        
+       
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.printf("Input number %d: ", i + 1);
+            numbers[i] = scanner.nextInt();
+        }
+        
+        
+        int oddCount = 0;
+        String oddNumbers = ""; 
+        
+       
+        for (int num : numbers) {
+            if (num % 2 != 0) { 
+                oddCount++;
+                oddNumbers += num + " "; 
+            }
+        }
+        
+        
+        System.out.println("");
+        System.out.println("There are " + oddCount + " odd number(s).");
+        System.out.println("List of odd number(s): " + oddNumbers.trim());
+        
+        scanner.close(); 
+    }
+}
