00716617db40df12a30a3a5d7e1c168350792ca7
SukonlawatT <sugornawat.0903@gmail.com>
2025-01-20 11:02:50 +0700

    Lap06

--- JavaLabsheetAll/Labsheet06/LabSheet06_ClassandObject/src/LoanCalculator.java
+++ JavaLabsheetAll/Labsheet06/LabSheet06_ClassandObject/src/LoanCalculator.java
@@ -1,30 +1,32 @@
-
 public class LoanCalculator 
 {
-	private String name;
-	private double price;
+    private String name;
+    private double price;
     private double interestRate;
     private double years;
-    
-    public void setLoanDetails(String productName, double dbprice, double dbinterestRate, int dbyears)
+
+    public void setLoanDetails(String productName, double dbprice, double dbinterestRate, int dbyears) 
     {
-    	name = productName;
-    	price = dbprice;
-    	interestRate  =dbinterestRate;
-    	years = dbyears;
+        name = productName;
+        price = dbprice;
+        interestRate = dbinterestRate;
+        years = dbyears;
     }
-    public double calculateMonthlyPayment()
+
+    public double calculateMonthlyPayment() 
     {
-    	double month = years *12;
-    	double interestRate = (interestRate/12)/100;
-    	
+        double month = years * 12;
+        double interestRatee = (interestRate / 12) / 100;
+        return (price * interestRatee * (Math.pow((1 + interestRatee), month))) /
+               ((Math.pow((1 + interestRatee), month)) - 1);
     }
-    public void displayLoanDetails()
+
+    public void displayLoanDetails() 
     {
-    	System.out.println("Product: "+ name);
-    	System.out.println("Price: "+ price);
-    	System.out.println("Interest Rate: "+ interestRate);
-    	System.out.println("Loan Poyment: "+ years);
-    	System.out.println("Monthly Poyment: "+ calculateMonthlyPayment());
+        System.out.println("Product: " + name);
+        System.out.println("Price: " + price);
+        System.out.println("Interest Rate: " + interestRate + "%");
+        System.out.println("Loan Period: " + years + " years");
+        System.out.println("Monthly Payment: " + calculateMonthlyPayment());
     }
-}
+}    
---
+++ JavaLabsheetAll/Labsheet06/LabSheet06_ClassandObject/src/TestLoanCalculator.java
@@ -0,0 +1,14 @@
+
+public class TestLoanCalculator 
+{
+
+	public static void main(String[] args) 
+	{
+		LoanCalculator lc1 = new LoanCalculator();
+		
+		lc1.setLoanDetails("Laptop", 50000, 5, 2);
+		lc1.displayLoanDetails();
+
+	}
+
+}
